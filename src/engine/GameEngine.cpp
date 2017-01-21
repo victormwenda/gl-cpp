5d1a09218bda3d3250e7f76d6a0c081d1783333f
Victor Mwenda <vmwenda.vm@gmail.com>
2017-01-21 18:24:22 +0300

    Created Game Engine
    This class is responsible for opening a OpengGL window, and drawing
    items to the window

---
+++ src/engine/GameEngine.cpp
@@ -0,0 +1,18 @@
+/*
+ * GameEngine.cpp
+ *
+ *  Created on: Jan 21, 2017
+ *      Author: victor
+ */
+
+#include "GameEngine.h"
+
+GameEngine::GameEngine() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GameEngine::~GameEngine() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/engine/GameEngine.h
@@ -0,0 +1,17 @@
+/*
+ * GameEngine.h
+ *
+ *  Created on: Jan 21, 2017
+ *      Author: victor
+ */
+
+#ifndef GAMEENGINE_H_
+#define GAMEENGINE_H_
+
+class GameEngine {
+public:
+	GameEngine();
+	virtual ~GameEngine();
+};
+
+#endif /* GAMEENGINE_H_ */
