da325e437cf7c311be4c2135a3a38c29a7474023
Victor Mwenda <vmwenda.vm@gmail.com>
2017-01-21 18:20:37 +0300

    Test source file to test C++ compiler configuration

---
+++ src/gl-cpp.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : gl-cpp.cpp
+// Author      : Victor Mwenda <vmwenda.vm@gmail.com>
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Sample OpenGL Game in C++" << endl; // prints Sample OpenGL Game in C++
+	return 0;
+}
